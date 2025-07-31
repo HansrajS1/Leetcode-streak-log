d6e2993fd507a9cfd12cdedffa988b5c10e2d26c
HANS RAJ <155075212+HansrajS1@users.noreply.github.com>
2025-07-31 08:51:41 +0000

    [LeetCode Sync] Runtime - 3 ms (53.31%), Memory - 42.2 MB (78.21%)

--- submissions/0043-multiply-strings/solution.java
+++ submissions/0043-multiply-strings/solution.java
@@ -1,30 +1,25 @@
 class Solution {
     public String multiply(String num1, String num2) {
-            if (num1.equals("0") || num2.equals("0")) {
-            return "0";
-        }
+        if(num1.equals("0") || num2.equals("0")) return "0";
 
-        int len1 = num1.length();
-        int len2 = num2.length();
-        int[] result = new int[len1 + len2]; 
+        int m = num1.length(), n = num2.length();
+        int[] result = new int[m + n];
+        
+        for(int i = m - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+            for(int j = n - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
 
-        for (int i = len1 - 1; i >= 0; i--) {
-            int digit1 = num1.charAt(i) - '0';
-            for (int j = len2 - 1; j >= 0; j--) {
-                int digit2 = num2.charAt(j) - '0';
+                int mul = d1 * d2;
+                int sum = mul + result[i + j + 1]; 
 
-                int mul = digit1 * digit2;
-                int pos1 = i + j, pos2 = i + j + 1;
-
-                int sum = mul + result[pos2];
-
-                result[pos2] = sum % 10;
-                result[pos1] += sum / 10;
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
             }
-        }
+        }    
         StringBuilder sb = new StringBuilder();
-        for (int num : result) {
-            if (sb.length() == 0 && num == 0) continue;
+        for(int num : result) {
+            if(sb.length() == 0 && num == 0) continue;
             sb.append(num);
         }
 
