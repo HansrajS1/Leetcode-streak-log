9771a3484274bf9176f72cc9de07a0c513454a4f
HANS RAJ <155075212+HansrajS1@users.noreply.github.com>
2026-07-28 15:45:29 +0000

    [LeetCode Sync] Runtime - 187 ms (91.69%), Memory - 274.2 MB (57.66%)

---
+++ submissions/0211-design-add-and-search-words-data-structure/solution.java
@@ -0,0 +1,57 @@
+class WordDictionary {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd = false;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+            curr = curr.children[index];
+        }
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int pos, TrieNode node) {
+        if (node == null) return false;
+
+        if (pos == word.length()) {
+            return node.isEnd;
+        }
+
+        char c = word.charAt(pos);
+
+        if (c == '.') {
+            for (TrieNode child : node.children) {
+                if (child != null && dfs(word, pos + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return dfs(word, pos + 1, node.children[c - 'a']);
+        }
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
