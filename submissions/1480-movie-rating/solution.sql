76da6dde05b87b3fcd2c3ef9614720510013ac5b
HANS RAJ <155075212+HansrajS1@users.noreply.github.com>
2026-08-26 13:16:19 +0000

    [LeetCode Sync] Runtime - 1603 ms (32.70%), Memory - 0.0B (100.00%)

--- submissions/1480-movie-rating/solution.sql
+++ submissions/1480-movie-rating/solution.sql
@@ -3,9 +3,9 @@
     SELECT u.name AS results
     FROM Users u
     JOIN MovieRating mr
-    ON u.user_id = mr.user_id
+        ON u.user_id = mr.user_id
     GROUP BY u.user_id, u.name
-    ORDER BY COUNT(mr.movie_id) DESC, u.name
+    ORDER BY COUNT(*) DESC, u.name ASC
     LIMIT 1
 )
 
@@ -15,10 +15,10 @@ UNION ALL
     SELECT m.title AS results
     FROM Movies m
     JOIN MovieRating mr
-    ON m.movie_id = mr.movie_id
-    WHERE created_at >= '2020-02-01'
-      AND created_at < '2020-03-01'
+        ON m.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01'
+      AND mr.created_at < '2020-03-01'
     GROUP BY m.movie_id, m.title
-    ORDER BY AVG(mr.rating) DESC, m.title
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
     LIMIT 1
 );
